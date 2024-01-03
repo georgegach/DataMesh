5e719ec8f0a98e2d70b0e4e6cde167dfabc6e3f2
George Gach <georgegach@outlook.com>
2024-01-03 18:29:06 +0400

    Streamlit added, OpenAPI examples to test against

---
+++ docs/streamlit/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from datamesh.contract import Validator
+import yaml
+import tempfile
+
+def validate_yaml(yaml_content):
+    validation = Validator(yaml_content)
+    return validation.is_valid(), validation.errors
+
+# Streamlit interface
+st.title("✅ ODCS Data Contract Validator")
+st.markdown("> The following streamlit app uses [DataMesh](https://pypi.org/project/DataMesh/) python library underneath to validate *.yaml contracts* against [Open Data Contract Standard](https://github.com/bitol-io/open-data-contract-standard) JSON Schema, part of Linux Foundation.")
+
+st.markdown("___")
+
+uploaded_file = st.file_uploader("Choose a YAML file containing data contract", type="yaml")
+
+if uploaded_file is not None:
+
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".yaml") as fp:
+        fp.write(uploaded_file.getvalue().decode("utf-8"))
+        fp.seek(0)
+
+        with open(fp.name, 'r') as file:
+
+            yaml_content = yaml.safe_load(file)
+            is_valid, errors = validate_yaml(yaml_content)
+
+            if is_valid:
+                st.success("💚 The YAML file is valid.")
+            else:
+                st.error("🚩 Validation failed. Errors:")
+                st.write(errors)
